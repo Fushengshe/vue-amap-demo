1e1a93adae5942d8d2eda71d1e83a8fd37caf3a3
周宝 <baobaozhou199606@gmail.com>
2017-06-10 00:43:35 +0800

    提示Maker is not a constructor

--- src/components/map/index.vue
+++ src/components/map/index.vue
@@ -1,18 +1,54 @@
 <template>
-    <div class="map">
-      <h3 class="title">{{ msg }}</h3>
-      <div class="amap-wrapper">
-        <!-- 注意：vid不要重复 -->
-        <el-amap :vid="'amap-vue'"></el-amap>
+  <div class="map">
+    <h3 class="title">{{ msg }}</h3>
+    <div class="amap-wrapper">
+      <!-- 注意：vid不要重复 -->
+      <el-amap
+        :vid="amap-vue"
+        :center="center"
+        :map-manager="amapManager"
+        :zoom="zoom"
+        :events="events"
+        class="amap-demo"
+      ></el-amap>
+      <div class="toolbar">
+        <button @click="add()">add market</button>
       </div>
     </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
+  /** 注意在这里需要引入 AMap，这一点和官方给的文档不是很一样
+   * 应该是因为es-lint的原因，会在events中显示AMap未定义
+   */
+  import AMap from 'vue-amap'
+  /* eslint-disable no-duplicate-imports */
+  import VueAMap from 'vue-amap'
+  let amapManager = new VueAMap.AMapManager()
+  export default{
     data () {
       return {
-        msg: 'this is the map components based on vue-amap'
+        zoom: 12,
+        center: [121.59996, 31.197646],
+        amapManager,
+        events: {
+          init (o) {
+            let marker = new AMap.Marker({
+              position: [121.59996, 31.197646]
+            })
+            marker.setMap(o)
+          }
+        }
+      }
+    },
+    methods: {
+      add () {
+        let o = amapManager.getMap()
+        let marker = new AMap.Marker({
+          position: [121.59996, 31.177646]
+        })
+        marker.setMap(o)
       }
     }
   }
@@ -21,7 +57,9 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus" rel="stylesheet/stylus">
   .amap-wrapper {
-    width: 500px;
-    height: 500px;
+
+    .amap-demo {
+      height 300px
+    }
   }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import Map from '../components/map'
+import Map from '../components/map/index.vue'
 Vue.use(Router)
 
 export default new Router({
