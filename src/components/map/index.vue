3da21956578167bd6919307c2dd93c63674da483
周宝 <baobaozhou199606@gmail.com>
2017-06-10 17:13:55 +0800

    添加了折线，但是无法进行编辑

--- src/components/map/index.vue
+++ src/components/map/index.vue
@@ -7,14 +7,16 @@
         :center="center"
         :zoom="zoom"
         class="amap-demo">
-        <el-amap-ground-image
-          v-for="(groundimage, index) in groundimages"
-          :index="index"
-          :url="groundimage.url"
-          :bounds="groundimage.bounds"
-          :events="groundimage.events"
-        ></el-amap-ground-image>
+        <el-amap-polyline
+          :editable="polyline.editable"
+          :path="polyline.path"
+          :events="polyline.events"
+        ></el-amap-polyline>
       </el-amap>
+      <div class="tool-bar">
+        <el-button type="primary" @click="changeEditable">edit the line</el-button>
+        <!--<el-button type="danger" @click="changeEditable">disable edit the line</el-button>-->
+      </div>
     </div>
   </div>
 </template>
@@ -25,22 +27,31 @@
     data () {
       return {
         msg: 'vue-amap say hello',
-        zoom: 7,
+        zoom: 12,
         center: [121.5273285, 31.21515044],
-        groundimages: [
-          {
-            url: '//faas.elemecdn.com/desktop/media/img/appqc.95e532.png',
-            bounds: [[121.5273285, 31.21515044], [122.9273285, 32.31515044]],
-            events: {
-              click: () => {
-                alert('click groundimage')
-              }
-            }
+        polyline: {
+          path: [[121.5389385, 31.21515044], [121.5389385, 31.29615044], [121.5273285, 31.21515044]],
+          events: {
+            click (e) {
+              console.log(e)
+              alert('your click the polyline')
+            },
+            end: (e) => {
+              let newPath = e.target.getPath().map(point => [point.lng, point.lat])
+              console.log(newPath)
+            },
+            editable: false
           }
-        ]
+        }
       }
     },
-    methods: {}
+    methods: {
+      changeEditable () {
+        this.polyline.editable = !this.polyline.editable
+        console.log('index.vue line 53')
+        console.log(this.polyline.editable)
+      }
+    }
   }
 </script>
 
