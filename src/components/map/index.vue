5670abd423a1277e058d8fcb9e2b8f46fb51012f
周宝 <baobaozhou199606@gmail.com>
2017-06-11 04:20:04 +0800

    使用类型插件

--- src/components/map/index.vue
+++ src/components/map/index.vue
@@ -3,48 +3,13 @@
     <h3 class="title">{{ msg }}</h3>
     <div class="amap-wrapper">
       <el-amap
-        :vid="'amap-vue'"
+        vid="amap-vue"
         :map-manager="amapManager"
         :center="center"
         :zoom="zoom"
+        :plugin="plugins"
         class="amap-demo">
-        <el-amap-marker
-          v-for="(marker, index) in markers"
-          :key="index"
-          :position="marker.position"
-          :event="marker.events"
-          :visible="marker.visible"
-          :draggable="marker.draggable"
-        ></el-amap-marker>
       </el-amap>
-      <div class="toolbar">
-        <el-button
-          class="toolbar-button"
-          @click="toggleVisible">
-          toggle first marker
-        </el-button>
-        <el-button
-          class="toolbar-button"
-          @click="changePosition">
-          change position
-        </el-button>
-        <el-button
-          class="toolbar-button"
-          @click="changeDraggle">
-          change draggle
-        </el-button>
-        <el-button
-          type="primary"
-          class="toolbar-button"
-          @click="addMarker">
-          add marker
-        </el-button>
-        <el-button
-          class="toolbar-button"
-          @click="removeMarker">
-          remove marker
-        </el-button>
-      </div>
     </div>
   </div>
 </template>
@@ -60,49 +25,22 @@
         amapManager,
         zoom: 14,
         center: [121.5273285, 31.21515044],
-        markers: [
-          {
-            position: [121.5273285, 31.21515044],
-            events: {
-              click: () => {
-                alert('your click me')
-              },
-              dragend: (e) => {
-                // 使用es6中额解构赋值
-                const {lng, lat} = e.target.getPosition()
-                this.markers[0].position = [lng,lat]
-              }
-            },
-            visible: true,
-            draggable: false
+        plugins: [{
+          pName: 'AMap.MapType',
+          defaultType: 0,
+          // 显示交通
+          showTraffic: false,
+          // 显示路网
+          showRoad: false,
+          events: {
+            init (instance) {
+              console.log(instance)
+            }
           }
-        ]
+        }]
       }
     },
-    methods: {
-      changePosition () {
-        let position = this.markers[0].position
-        this.markers[0].position = [position[0] + 0.002, position[1] - 0.002]
-      },
-      changeDraggle () {
-        let draggable = this.markers[0].draggable
-        this.markers[0].draggable = !draggable
-      },
-      toggleVisible () {
-        let visibleVar = this.markers[0].visible
-        this.markers[0].visible = !visibleVar
-      },
-      addMarker () {
-        let marker = {
-          position: [121.5273285 + (Math.random() - 0.5) * 0.02, 31.21515044 + (Math.random() - 0.5) * 0.02]
-        }
-        this.markers.push(marker)
-      },
-      removeMarker () {
-        if (!this.markers.length) return
-        this.markers.splice(this.markers.length - 1, 1)
-      }
-    }
+    methods: {}
   }
 </script>
 
