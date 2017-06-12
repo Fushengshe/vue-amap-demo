dd4bbb8c0421e06111db46938d913d7285a8450c
周宝 <baobaozhou199606@gmail.com>
2017-06-12 21:54:19 +0800

    walking err

--- src/components/map/index.vue
+++ src/components/map/index.vue
@@ -17,13 +17,18 @@ export default {
           center: [116.397428, 39.90923], // 地图中心点
           zoom: 13 // 地图显示的缩放级别
         })
-      let walking = new AMap.Walking({
-        map: map,
-        panel: 'panel'
-      })
-      console.log(walking)
-      this.$nextTick(() => {
-        walking.search([116.399028, 39.845042], [116.436281, 39.880719])
+      AMap.service(['AMap.walking'], function () {
+        let MWalk = new AMap.Walking({
+          map: map,
+          panel: 'panel'
+        })
+        MWalk.search([
+          {keyword: '北京市地震局(公交站)'},
+          {keyword: '亦庄文化园(地铁站)'}
+        ], function (status, result) {
+
+        })
+        console.log(MWalk)
       })
     })
   }
