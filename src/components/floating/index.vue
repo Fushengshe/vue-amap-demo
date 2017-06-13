62e0bdeb0c9edb85248cd91b90af4ffd2afabaa7
周宝 <baobaozhou199606@gmail.com>
2017-06-13 17:04:41 +0800

    add floating component

---
+++ src/components/floating/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="floating">
+        <p>{{msg}}</p>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'template',
+    data () {
+      return {
+        msg: 'this is the template component!'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .floating{
+    font-size 1rem
+  }
+</style>
